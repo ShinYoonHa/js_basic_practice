85701b81397abf612fad622452eb5a822ad68e13
SinYoonHa <idc06012@naver.com>
2023-07-15 23:35:03 +0900

    already defined parameter

--- 05/js/add2.js
+++ 05/js/add2.js
@@ -1,10 +1,9 @@
 var num1 = Number(prompt("첫 번째 숫자를 입력하시오"));
-var num2 = Number(prompt("첫 번째 숫자를 입력하시오"));
 var num3 = Number(prompt("세 번째 숫자를 입력하시오"));
 
-addNumber(num1, num2, num3);
+addNumber(num1, num3);
 
-function addNumber(a,b,c) {
+function addNumber(a,b,c=3) {
     var sum = a+b+c;
     alert(sum);
 }
