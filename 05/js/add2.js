80ea417b1c55575157250caa93e070ac6c9688cf
SinYoonHa <idc06012@naver.com>
2023-07-15 23:35:50 +0900

    already defined parameter

--- 05/js/add2.js
+++ 05/js/add2.js
@@ -3,7 +3,7 @@ var num3 = Number(prompt("세 번째 숫자를 입력하시오"));
 
 addNumber(num1, num3);
 
-function addNumber(a,b,c=3) {
+function addNumber(a,b,c=3) { //b는 미리 선언하고 C는 안할 수 없음. 순서 상 안됨
     var sum = a+b+c;
     alert(sum);
 }
